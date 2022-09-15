f34275fcbfe473e72d878bc4054537f4d02c3920
jacobejamesmark22 <112452473+jacobejamesmark22@users.noreply.github.com>
2022-09-15 16:07:47 +0800

    Add files via upload

---
+++ CMSC203_ACTIVITY1_JACOBE.java
@@ -0,0 +1,21 @@
+
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        String Name =  "James Mark Jacobe";
+        String YOld = "19";
+        String GPA  = "1.50";
+        String BTyp = "O+";
+
+        System.out.println("\nHi, my name is "+Name+".");
+        System.out.println("\nI am "+YOld+" years old.");
+        System.out.println("\nMy GPA is "+GPA+".");
+        System.out.println("\nMy  is "+BTyp+".\n");
+       }
+    }
+// My Refference 
+// Hi, my name is Regina Castillo.
+// I am 20 years old.
+// My GPA is 2.25.
+// My blood type is B.       
